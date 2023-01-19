3d91af483a21f9209f35ecbf0877d8c69eadae6a
sbata1984 <h.hicham@hotmail.de>
2023-01-19 21:00:38 +0100

    first commit

---
+++ junit-jupiter-api/src/test/java/triagle/TriangleTest.java
@@ -0,0 +1,11 @@
+package triagle;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.*;
+
+public class TriangleTest {
+	@Test
+	public void test
+	
+	
+}
